66835c86300ed644237cc3676d09124f7a9343ed
Mohamed Boukouch <boukouchmohamed7@gmail.com>
2023-09-13 13:52:27 +0100

    first commit

--- lib/app/config/images/app_images.dart
+++ lib/app/config/images/app_images.dart
@@ -8,5 +8,6 @@ class AppMessage {
   static const String spl_2 = "assets/splashimg/SPL2.jpg";
   static const String spl_3 = "assets/splashimg/SPL3.jpg";
   static const String logo = "assets/logo/logo-project.png";
-
+  static const String email_icon = "assets/icons/icon2.png";
+  static const String password_icon = "assets/icons/icon3.png";
 }
